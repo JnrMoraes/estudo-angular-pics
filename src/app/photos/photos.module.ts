e6e946c5953fbc0f79b34185aa3d2df0f2bf910e
juniormoraes <contatomoraesjr@gmail.com>
2021-03-08 20:46:38 -0300

    Single Page Application e Rotas, ora carrega um componente ou outro

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
   declarations: [ 
     PhotoComponent, 
-    PhotoListComponent 
+    PhotoListComponent, 
+    PhotoFormComponent 
   ],
   imports: [ HttpClientModule,
   CommonModule ]

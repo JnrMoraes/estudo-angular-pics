153ea15b7cc05e7ef435495b39ad5480ebe19b3d
juniormoraes <contatomoraesjr@gmail.com>
2021-03-08 21:28:23 -0300

    Paramentrizando o segmentos de rotas

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Photo } from "./photo";
 
-const API = 'http://localhost:3000/';
+const API = 'http://localhost:3000';
 
 @Injectable({ providedIn: 'root'})
 export class PhotoService {
@@ -13,7 +13,7 @@ export class PhotoService {
   listFromUser(userName: string){
 
      return this.http
-    .get<Photo[]>( API + 'flavio/photos');
+    .get<Photo[]>( API + '/' + userName + '/photos');
     
 
   }

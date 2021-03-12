366b331c3cb2cd60c56aaa6a8451180f41367be3
juniormoraes <contatomoraesjr@gmail.com>
2021-03-11 21:32:34 -0300

    Inicio de evento Binding

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -11,6 +11,7 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: any[] = []; 
+  filter: String = '';
   
   constructor(
     private photoService: PhotoService,
